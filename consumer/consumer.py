f8e1158c1ed0b5b304cda8fa128cd4b7d377329b
Lord Samedi <ignacio.mediaz@gmail.com>
2024-05-26 00:06:08 -0400

    new db 03

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,19 +1,19 @@
 from aiokafka import AIOKafkaConsumer
 import asyncio
 import json
-import psycopg2
+from db import insert_data  # Importar la función insert_data desde db.py
 
 async def consume():
     consumer = AIOKafkaConsumer(
         'my_topic', 'my_other_topic',
-        bootstrap_servers='localhost:9092',
+        bootstrap_servers='kafka:9092',
         group_id="my-group")
     await consumer.start()
     try:
-        
         async for msg in consumer:
-            print("consumed: ", msg.topic, msg.partition, msg.offset,
-                  msg.key, msg.value, msg.timestamp)
+            data = json.loads(msg.value)
+            print("Datos recibidos:", data)
+            insert_data(data)  # Llamar a la función insert_data desde db.py
     finally:
         await consumer.stop()
 
